997d46b3d4034a470e58a8d6492fc59543f0ba35
Leonardo Lana <leolucas.lana@hotmail.com>
2015-12-01 00:47:59 -0200

    Add unit testing project with xunit.net

---
+++ MemorySharing/MemorySharingUnitTests1/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MemorySharingUnitTests1
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
